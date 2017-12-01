4d711a2850c6843c11f37cf390ebecbabe2029ac
EugeneKorniets <evgeniy.korniets@gmail.com>
2017-12-01 20:04:51 +0300

    Sliders

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+/* menu open/close control and no-js handler   start */
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -12,3 +13,52 @@ navToggle.addEventListener('click', function() {
 		navMain.classList.remove('main-nav--opened');
 	}
 });
+/* menu open/close control and no-js handler   end */
+
+/* feedback-slider scroll controls handler   start */
+var feedbackSliderInputs = document.getElementsByClassName('feedback__control');
+
+setInterval (feedbackSliderNext, 8000); // автоскролл фидбек слайдов
+
+/* Функциb смены слайдов (previous and next)
+ * запускаем цикл поиска активного слайда в коллекции
+ * проверяем активность текущего слайда
+ * если слайд последний, активируем первый
+ * иначе активируем следующий
+*/
+function feedbackSliderPrev() {
+    for (var i = feedbackSliderInputs.length - 1; i >= 0; i--) {
+        if (feedbackSliderInputs[i].checked == true) {
+            if(i == 0) {
+                feedbackSliderInputs[feedbackSliderInputs.length - 1].checked = true;
+                return;
+            } else {
+                feedbackSliderInputs[i-1].checked = true;
+                return;
+            }
+        }
+    }
+}
+
+function feedbackSliderNext() {
+    for (var i = 0; i <= feedbackSliderInputs.length - 1; i++) {
+        if (feedbackSliderInputs[i].checked == true) {
+            if(i == feedbackSliderInputs.length - 1) {
+                feedbackSliderInputs[0].checked = true;
+                return;
+            } else {
+                feedbackSliderInputs[i+1].checked = true;
+                return;
+            }
+        }
+    }
+}
+
+// регистрация обработчиков событий для переключателей фидбек-слайдов на десктопной версии
+
+var feedbackPrevSlide = document.querySelector('.feedback__prev');
+var feedbackNextSlide = document.querySelector('.feedback__next');
+
+feedbackNextSlide.addEventListener('click', feedbackSliderNext);
+feedbackPrevSlide.addEventListener('click', feedbackSliderPrev);
+/* feedback-slider scroll controls handler   end */
